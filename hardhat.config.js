5e25ed4b124cbc4c7d1437836defaf41f1c307ed
statan7 <themard.529@gmail.com>
2022-03-02 16:14:58 +0530

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+module.exports = {
+  solidity: '0.8.10',
+  networks: {
+    mumbai: {
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    }
+  },
+};
